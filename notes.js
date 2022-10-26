352b68bcd6c385738116b21a4642fd44694b3a52
diego-salves <diegosantos57@outlook.com.br>
2022-10-26 00:05:53 -0300

    .

---
+++ notes.js
@@ -0,0 +1,53 @@
+/* variáveis e tipos de dados
+
+igual no python
+*/
+
+/* Estruturas condicionais
+if
+else if
+else
+
+muda apenas o nome
+
+if (condição){
+    faz x
+} else if (outra condicao) {
+    faz y
+} else {
+    faz z
+}
+
+switch case(var qualquer):
+    case 'm'
+        faz x
+    break;
+    case 'z':
+        faz y
+    break;
+    default:
+        faz z
+    
+*/
+
+/*
+Operações matemáticas
+
+só funcionam com data types int ou float, igual no python
+*/
+
+/*Estruturas de repetição
+for 
+while
+
+do-while
+
+*/
+
+/*
+operadores lógicos
+
+&& and
+|| or
+!= not
+*/
