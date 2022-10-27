abdd8e215ee9ec6413e08f3625cc00ff309ad255
diego-salves <diegosantos57@outlook.com.br>
2022-10-27 09:02:17 -0300

    .

--- My Notes/notes.js
+++ My Notes/notes.js
@@ -1,9 +1,11 @@
+//for
 for (var i = 1; i <= 10; i++){
     console.log(i)
 }
 
 var x = 1;
 
+//while
 while (x <= 10){
     console.log(x)
     x++
@@ -11,7 +13,30 @@ while (x <= 10){
 
 x = 1
 
+//do
 do {
  console.log(x);
  x++;   
 } while (x <= 10);
+
+//forEach
+
+var cursos = [
+    'Javascript',
+    'Python',
+    'Java',
+    'React',
+    'SQL',
+    'Next'
+]
+
+function print(curso, indice, array){
+    console.log(`O curso ${curso} está no índice ${indice}`)
+}
+
+cursos.forEach(print)
+
+//passa 3 parametros, valor, indice e array
+//usando lambda
+
+cursos.forEach(function(curso,indice){console.log(`${indice} - ${curso}`)})
