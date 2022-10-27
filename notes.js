0416e083df3c5d6b6fd328731f1e714834fb4152
diego-salves <diegosantos57@outlook.com.br>
2022-10-26 22:51:27 -0300

    .

---
+++ notes.js
@@ -0,0 +1,17 @@
+for (var i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+var x = 1;
+
+while (x <= 10){
+    console.log(x)
+    x++
+} 
+
+x = 1
+
+do {
+ console.log(x);
+ x++;   
+} while (x <= 10);
