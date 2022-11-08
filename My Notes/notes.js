3d815e7c685e774fa9d042b941855913485dca4b
diego-salves <diegosantos57@outlook.com.br>
2022-11-08 00:16:43 -0300

    .

--- My Notes/notes.js
+++ My Notes/notes.js
@@ -79,4 +79,62 @@ console.log(resultado)
 
 // diferença do map é que traz um array menor geralmente, por filtrar, mas filter não executa alterações no array
 
+// HOISTING = Iça a variável caso seja declarada como var 
 
+console.log(nome)
+
+var nome = "Diego";
+
+console.log(nome)
+
+// reduce
+
+var precos = [4.5, 3.78, 9.78, 1.34]
+
+var soma = 0
+
+for (var i = 0; i < precos.length; i++){
+    soma = soma + precos[i]
+}
+
+console.log(soma)
+
+var soma2 = 0
+
+precos.forEach(valor => soma2 += valor);
+
+console.log(soma2)
+
+console.log(precos)
+
+function multiplicar(valor){
+    return valor * 3;
+}
+
+var m = 1
+
+precos.forEach(element => m *= element);
+console.log(m)
+
+var resultado = precos.reduce(multiplicar);
+console.log(resultado)
+
+function adicionar_taxa(valor){
+    return valor + 5;
+}
+
+function divide_por_2(anterior, atual){
+    return (anterior / 2) + atual
+}
+
+console.log(precos)
+var resultado2 = precos.map(adicionar_taxa).reduce(divide_por_2);
+
+console.log(resultado2)
+
+function preco_maior_que_4(valor){
+    return valor > 4;
+}
+
+ret = precos.filter(element => element > 4).map(element => element + 5).reduce((anterior,atual) => {return (anterior / 2) + atual});
+console.log(ret);
