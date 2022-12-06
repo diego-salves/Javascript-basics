8f1536bb2826380689f08e5242e217a65168e578
diego-salves <diegosantos57@outlook.com.br>
2022-12-05 22:43:15 -0300

    ex-07 - extended class and getters and setters created

--- Exercises/advanced/ex-07.js
+++ Exercises/advanced/ex-07.js
@@ -1,6 +1,61 @@
+class Usuario {
+    constructor (email, senha){
+        this._email = email;
+        this._senha = senha;
+    }
+
+    get email(){
+        return this._email;
+    } 
+
+    get senha(){
+        return this._senha;
+    }
+
+    set email(email){
+        this._email = email;
+    }
+
+    set senha(senha){
+        this._senha = senha;
+    }
+
+    ehAdmin(){
+        if(this.admin == true){
+            console.log(true);
+        } else {
+            console.log(false);
+        }
+    }
+}
+
+class Admin extends Usuario {
+    constructor(email, senha){
+        super(email, senha);
+        this.admin = true;
+    }
+
+    get email(){
+        return this._email;
+    } 
+
+    get senha(){
+        return this._senha;
+    }
+
+    set email(email){
+        this._email = email;
+    }
+
+    set senha(senha){
+        this._senha = senha;
+    }
+}
+
+
 let usuario1 = new Usuario("joao@gmail.com", "senhasegura");
 let usuario2 = new Admin("maria@gmail.com", "senhasupersegura");
 
-console.log(usuario1.ehAdmin()); //false
-console.log(usuario2.ehAdmin()); //true
+usuario1.ehAdmin(); //false
+usuario2.ehAdmin(); //true
 
